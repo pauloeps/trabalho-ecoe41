0d9a1171e547138078336885191da59f269846a2
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-11-20 00:05:56 -0300

    pouca coisa

--- arvorebinaria/arvorebin.py
+++ arvorebinaria/arvorebin.py
@@ -1,9 +1,17 @@
-class No():
-    def __init__(self,val):
+class No:
+    def __init__(self,val,canvas,x0,y0,x1,y1):
         self.valor = val
         self.direita = None
         self.esquerda = None
-
-class ArvoreBin():
+        self.oval = canvas.create_oval(x0, y0, x1, y1, width = 2)
+        self.text = canvas.create_text((x1-x0)/2, (y1-y0)/2, text = val)
+    def insertDir(self,valor):
+        self.direita = No(valor)
+    def insertEsq(self,valor):
+        self.esquerda = No(valor)
+    
+class ArvoreBin:
     def __init__(self,raiz):
         self.root = raiz
+    
+    
