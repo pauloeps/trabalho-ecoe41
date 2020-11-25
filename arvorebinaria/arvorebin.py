baa70a8a56c5db28df843dbda07cfcea3324fcf9
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-11-25 17:52:11 -0300

    Arvorebin

--- arvorebinaria/arvorebin.py
+++ arvorebinaria/arvorebin.py
@@ -1,10 +1,10 @@
 class No:
-    def __init__(self,val,canvas,x0,y0,x1,y1):
+    def __init__(self,val,circ,text):
         self.valor = val
+        self.circulo = circ
+        self.text
         self.direita = None
         self.esquerda = None
-        self.oval = canvas.create_oval(x0, y0, x1, y1, width = 2)
-        self.text = canvas.create_text((x1-x0)/2, (y1-y0)/2, text = val)
     def insertDir(self,valor):
         self.direita = No(valor)
     def insertEsq(self,valor):
@@ -13,3 +13,26 @@ class No:
 class ArvoreBin:
     def __init__(self,raiz):
         self.root = raiz
+    def emOrdem(self,noAux):
+        if noAux is None:
+            return
+        self.emOrdem(noAux.esquerda)
+        print (noAux.valor, end = " ")
+        self.emOrdem(noAux.direita)
+        
+    def preOrdem(self,noAux):
+        if noAux is None:
+            return
+        print (noAux.valor,end = " ")
+        self.preOrdem(noAux.esquerda)
+        self.preOrdem(noAux.direita)
+        
+    def posOrdem(self,noAux):
+        if noAux is None:
+            return
+        self.posOrdem(noAux.esquerda)
+        self.posOrdem(noAux.direita)
+        print (noAux.valor, end = " ")
+
+class DrawCircle():
+    
